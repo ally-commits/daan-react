1d28377bb92a7da0a16ace7f59d6275c1cd86fb4
AlwinCrasta <alwyncrasta000@gmail.com>
2020-11-25 19:44:13 +0530

    chnages

--- src/components/HomePage/StartYour/StartYour.js
+++ src/components/HomePage/StartYour/StartYour.js
@@ -4,7 +4,28 @@ import styles from './StartYour.module.css'
 const StartYour = () => {
     return (
         <div className={styles.container}>
-            
+            <div className={styles.content}>
+                <p>START YOUR  FUNDRAISER RIGHT AWAY</p>
+
+                <div className={styles.blocks}>
+                    <div className={styles.block}>
+                        How much do you want to raise?
+                    </div>
+                    <div className={styles.block}>
+                        How much do you want to raise?
+                    </div>
+                    <div className={styles.block}>
+                        How much do you want to raise?
+                    </div>
+                    <div className={styles.block}>
+                        How much do you want to raise?
+                    </div>
+
+                    <button className={styles.button}>
+                        Submit
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
